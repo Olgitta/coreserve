49f088926c94668ed28c0bab1659c1295799d26f
Olgitta <olga.klimkin@gmail.com>
2024-12-28 22:44:57 +0200

    openapi

--- scripts/postman_to_openapi.js
+++ scripts/postman_to_openapi.js
@@ -3,6 +3,7 @@ const path = require('node:path');
 
 const postmanCollection = path.join(__dirname, '../coreserve.postman_collection.json');
 const outputJsonFile = path.join(__dirname, '../src/openapi/api-spec.json');
+const outputYamlFile = path.join(__dirname, '../src/openapi/api-spec.yaml');
 
 postmanToOpenApi(postmanCollection, outputJsonFile,
     {
@@ -13,6 +14,21 @@ postmanToOpenApi(postmanCollection, outputJsonFile,
     })
     .then((result) => {
         console.log(`OpenAPI generated: ${outputJsonFile}`);
+    })
+    .catch((err) => {
+        console.error('Error generating OpenAPI:', err);
+    });
+
+
+postmanToOpenApi(postmanCollection, outputYamlFile,
+    {
+        defaultTag: 'API',
+        replaceVars: true,
+        responseHeaders: false,
+        outputFormat: 'yaml',
+    })
+    .then((result) => {
+        console.log(`OpenAPI generated: ${outputYamlFile}`);
     })
     .catch((err) => {
         console.error('Error generating OpenAPI:', err);
